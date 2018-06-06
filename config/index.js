f6fb5bb3f9129b0ddf91324dcd6948f73f734a7f
PZYYY <15920515409@163.com>
2018-06-06 14:14:21 +0800

    2018-6-6 14:14:16

--- config/index.js
+++ config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: 'https://pzyyy.github.io/terminal-inline/',
 
     /**
      * Source Maps
--- src/router/index.js
+++ src/router/index.js
@@ -15,7 +15,7 @@ export default new Router({
   routes: [
     {
       path: '/',
-      redirect: '/main/AttendModel'
+      redirect: '/terminal-inline'
     },
     {
       path: '*',
